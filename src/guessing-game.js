4286250653b1ea56d1940ce51fc1dec7e910670c
DenisovaSS <114432399+DenisovaSS@users.noreply.github.com>
2023-10-29 12:41:32 +0200

    feat: add decision

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,21 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {}
 
-    setRange(min, max) {
+  setRange(min, max) {
+    this.minV = min;
+    this.maxV = max;
+  }
+  guess() {
+    return Math.round((this.maxV - this.minV) / 2 + this.minV);
+  }
 
-    }
+  lower() {
+    this.maxV = Math.round((this.maxV - this.minV) / 2 + this.minV);
+  }
 
-    guess() {
-
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+    this.minV = Math.round((this.maxV - this.minV) / 2 + this.minV);
+  }
 }
 
 module.exports = GuessingGame;
